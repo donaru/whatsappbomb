7d5d88a083ac15d30cdf7c321b30dcf39978d02b
DESKTOP-K3B2M7Q\Donald <chan_donald@Hotmail.com>
2018-04-30 19:48:20 +1000

    Updated sofware codes

--- BulkGroupCreator/BulkGroupCreator/Form1.Designer.cs
+++ BulkGroupCreator/BulkGroupCreator/Form1.Designer.cs
@@ -28,12 +28,183 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox_victim = new System.Windows.Forms.TextBox();
+            this.textBox_groupName = new System.Windows.Forms.TextBox();
+            this.textBox_number = new System.Windows.Forms.TextBox();
+            this.button_Go = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox_victim
+            // 
+            this.textBox_victim.Location = new System.Drawing.Point(156, 97);
+            this.textBox_victim.Name = "textBox_victim";
+            this.textBox_victim.Size = new System.Drawing.Size(144, 20);
+            this.textBox_victim.TabIndex = 0;
+            // 
+            // textBox_groupName
+            // 
+            this.textBox_groupName.Location = new System.Drawing.Point(156, 123);
+            this.textBox_groupName.Name = "textBox_groupName";
+            this.textBox_groupName.Size = new System.Drawing.Size(144, 20);
+            this.textBox_groupName.TabIndex = 1;
+            // 
+            // textBox_number
+            // 
+            this.textBox_number.Location = new System.Drawing.Point(156, 149);
+            this.textBox_number.Name = "textBox_number";
+            this.textBox_number.Size = new System.Drawing.Size(100, 20);
+            this.textBox_number.TabIndex = 2;
+            // 
+            // button_Go
+            // 
+            this.button_Go.BackColor = System.Drawing.Color.Red;
+            this.button_Go.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button_Go.Location = new System.Drawing.Point(174, 174);
+            this.button_Go.Name = "button_Go";
+            this.button_Go.Size = new System.Drawing.Size(96, 43);
+            this.button_Go.TabIndex = 3;
+            this.button_Go.Text = "Go";
+            this.button_Go.UseVisualStyleBackColor = false;
+            this.button_Go.Click += new System.EventHandler(this.button_Go_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(276, 175);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(99, 42);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Quit";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Firebrick;
+            this.label1.Location = new System.Drawing.Point(12, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(337, 25);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "登登登WhatsApp 無限群組Generator";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Maroon;
+            this.label2.Location = new System.Drawing.Point(45, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Contact名稱";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Maroon;
+            this.label3.Location = new System.Drawing.Point(45, 127);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Fake群組名稱";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Maroon;
+            this.label4.Location = new System.Drawing.Point(45, 153);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 16);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "群組數量";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, -1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(387, 64);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Brown;
+            this.label5.Location = new System.Drawing.Point(-3, 220);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(222, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "This software is for education purposes only.  ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.Brown;
+            this.label6.Location = new System.Drawing.Point(359, 220);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(28, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "v1.0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.MistyRose;
+            this.ClientSize = new System.Drawing.Size(387, 239);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button_Go);
+            this.Controls.Add(this.textBox_number);
+            this.Controls.Add(this.textBox_groupName);
+            this.Controls.Add(this.textBox_victim);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "登登登WhatsApp 無限群組Generator by Donaru";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox_victim;
+        private System.Windows.Forms.TextBox textBox_groupName;
+        private System.Windows.Forms.TextBox textBox_number;
+        private System.Windows.Forms.Button button_Go;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
     }
 }
 
--- BulkGroupCreator/BulkGroupCreator/Form1.cs
+++ BulkGroupCreator/BulkGroupCreator/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using AutoItX3Lib;
 
 namespace BulkGroupCreator
 {
@@ -15,6 +16,40 @@ namespace BulkGroupCreator
         public Form1()
         {
             InitializeComponent();
+
+        }
+
+        private void button_Go_Click(object sender, EventArgs e)
+        {
+            
+            String victim = textBox_victim.Text;
+            String groupName = textBox_groupName.Text;
+            int number = Convert.ToInt32(textBox_number.Text);
+
+            AutoItX3 _autoit = new AutoItX3();
+            _autoit.WinActivate("WhatsApp");
+
+            for (int i = 0; i < number; i++)
+            {
+                _autoit.Send("{CTRLDOWN}{SHIFTDOWN}n{CTRLUP}{SHIFTUP}");
+
+                _autoit.Send(victim);
+                _autoit.Send("{ENTER}");
+                _autoit.Send("{ENTER}");
+
+                String newGroupName = " " + groupName + " " + i.ToString();
+                _autoit.Send(newGroupName);
+                _autoit.Send("{ENTER}");
+
+                _autoit.Send("hi");
+                _autoit.Send("{ENTER}");
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
